184b174fcdce4b0a327f26499b40ff421d21ee96
Bill Edwards <winner.bill@gmail.com>
2025-10-08 09:28:14 +0000

    Ensure that venv and HF model cache stored on pod storage not on persistent network storage volume.

--- run_fine_tuning.py
+++ run_fine_tuning.py
@@ -1,4 +1,12 @@
+import os
+
 from configs.first_full_config import first_full_config
 from fine_tuning import run_fine_tuning
+
+os.environ['HF_HOME'] = '/root/.cache/huggingface'
+
+# Disable hf_transfer if not installed
+if 'HF_HUB_ENABLE_HF_TRANSFER' in os.environ:
+    del os.environ['HF_HUB_ENABLE_HF_TRANSFER']
 
 run_fine_tuning(first_full_config)
